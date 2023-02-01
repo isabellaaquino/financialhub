4276d59e8feea679d52ab9c48177a95afd750099
pedrodellolio <pedrohdellolio@gmail.com>
2023-02-01 12:32:41 -0300

    Removed unused files from react project

--- financialapp/frontend/src/App.tsx
+++ financialapp/frontend/src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Teste de html
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
