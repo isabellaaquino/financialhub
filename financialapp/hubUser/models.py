3ba0120671c49f023567509f7c670b896d5a5c2d
isabellaaquino <isabellaviviani6@gmail.com>
2023-02-06 15:39:08 -0300

    feature - adding new user attributes + pillow

--- financialapp/hubUser/models.py
+++ financialapp/hubUser/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 class HubUser(AbstractBaseUser):
-    username = models.CharField(max_length=250, blank=True)
+    avatar = models.ImageField(null=True, blank=True, upload_to="images/")
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField(
         unique=True, help_text="Your email is used for signing in. This cannot be changed later.",
         error_messages={'unique': "This email has already been used."}
